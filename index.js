c3f4d130afb7db2d780d2fbbbebf149980c2d6ec
Vladimir <wlrybas@gmail.com>
2021-01-01 20:45:00 +0300

    created farm with node.js

---
+++ index.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+//
+// Files
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `This is what we know about avokado: ${textIn} \n Created on ${Date.now()}`
+
+// fs.writeFileSync("./txt/input.txt", textOut);
+// console.log('File written')
+
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/stafffffrt.txt",  'utf-8', (err, data1) => {
+//   if (err) return console.log("Error 💥");
+//   fs.readFile(`./txt/${data1}.txt`,  'utf-8', (err, data2) => {
+//     console.log(data2);
+//       fs.readFile("./txt/append.txt",  'utf-8', (err, data3) => {
+//         console.log(data3);
+
+//         fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//           console.log('Your file has been written');
+//       })
+//     })
+//   })
+// });
+// console.log('Wiil read file');
+
+//
+//  Server
+
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+
+console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templateCard, el))
+      .join('');
+    const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === '/product') {
+    const product = dataObj[query.id];
+    const output = replaceTemplate(templateProduct, product);
+
+    res.end(output);
+    // API Page
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+    // Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found</h1>');
+  }
+});
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listeniiiiing');
+});
